014499952175e4cc7789b364ab9ff197e2d8501b
Josefina Mancilla <josefinanoemi4@gmail.com>
2018-04-05 14:57:16 -0500

    added get routes for regulars and baristas in app.js

--- public/js/app.js
+++ public/js/app.js
@@ -6,7 +6,9 @@ app.controller('MainController', ['$http', function($http) {
   this.user = {};
   this.registerForm = {};
   this.loginForm = {};
-  this.logged = false;
+  this.logged = false; //if true, add, edit and delete buttons will appear
+  this.baristas = []; //this array will hold all barista information
+  this.regulars = []; //this array will hold all regular information
 
   //LOGIN
   this.login = () => {
@@ -46,5 +48,31 @@ app.controller('MainController', ['$http', function($http) {
       this.logged = true;
     }
   }).catch(err => console.error('Catch:', err.message));
+
+  //LOAD ALL BARISTA INFORMATION
+  this.getBaristas = () => {
+    $http({
+      method: 'get',
+      url: '/baristas'
+    }).then(response => {
+      console.log(response.data);
+      this.baristas = response.data;
+    })catch(err => console.error('Catch:', err));
+  }
+
+  this.getBaristas();
+
+  //LOAD ALL REGULARS INFORMATION
+  this.getRegulars = () => {
+    $http({
+      method: 'get',
+      url: '/regulars'
+    }).then(response => {
+      console.log(response.data);
+      this.regulars = response.data;
+    }).catch(err => console.error('Catch:', err));
+  }
+
+  this.getRegulars();
 
 }]);
