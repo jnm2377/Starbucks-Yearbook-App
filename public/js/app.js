6260ec0f10236473f2f36edb72237794ba5843e2
Josefina Mancilla <josefinanoemi4@gmail.com>
2018-04-05 14:36:33 -0500

    added login http request to app.js

--- public/js/app.js
+++ public/js/app.js
@@ -5,9 +5,21 @@ app.controller('MainController', ['$http', function($http) {
 
   this.user = {};
   this.registerForm = {};
+  this.loginForm = {};
   this.logged = false;
 
   //LOGIN
+  this.login = () => {
+    $http({
+      method: '/post',
+      url: '/sessions/login',
+      data: this.loginForm
+    }).then(response => {
+      console.log(response.data);
+      this.user = response.data;
+      this.logged = true;
+    }).catch(err => console.error('Catch:', err.message));
+  }
 
   //REGISTER
   this.register = () => {
@@ -22,5 +34,17 @@ app.controller('MainController', ['$http', function($http) {
       this.logged = true;
     }).catch( err => console.error('Catch:', err.message));
   }
-  
+
+  //CHECK TO SEE IF USER IS LOGGED IN
+  $http({
+    method: 'get',
+    url: '/sessions'
+  }).then(response => {
+    if (response.data.user) {
+      this.user = response.data.user;
+      console.log(this.user);
+      this.logged = true;
+    }
+  }).catch(err => console.error('Catch:', err.message));
+
 }]);
